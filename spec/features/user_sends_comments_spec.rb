38eade96a871ee5db04e011da5f831f176be03eb
Mariana Naomi Yamada Castro <mariananycastro@gmail.com>
2020-03-04 17:12:25 -0300

    WIP comment

---
+++ spec/features/user_sends_comments_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+feature 'user creates comment' do
+  scenario 'successfully' do
+    login_as create(:user)
+    challenge = create(:challenge)
+
+    root_path
+    click_on 'Desafio 1'
+    fill_in 'Comentário', with: 'Meu comentário'
+    click_on 'Enviar'
+
+    expect(page).to have_css('#comment', text: 'Meu comentário')
+end
